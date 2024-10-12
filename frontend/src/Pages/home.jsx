15eac0d1014ffb176eaad331af22a220435cac4d
ahmada <amethgaye1401@gmail.com>
2024-10-12 16:00:11 +0000

    tester

--- frontend/src/Pages/home.jsx
+++ frontend/src/Pages/home.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
     <div>
-        <p className='text-2xl flex justify-center'>Hello world</p>
+      <p className="text-2xl flex justify-center">Hello world</p>
+      <p className="text-2xl flex justify-center">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus
+        pariatur nostrum ex possimus, inventore rerum minima impedit quod error
+        minus doloribus consectetur facere voluptas nulla repellendus, saepe
+        accusantium quam placeat. Commodi pariatur eius aperiam, tenetur placeat
+        fuga beatae distinctio explicabo.{" "}
+      </p>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
