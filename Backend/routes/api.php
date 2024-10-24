03af0e8273dffe23f2d0355ac3458746266ec8a6
Akkdevs47 <akkdevs47@gmail.com>
2024-10-24 14:06:34 +0000

    code and reset password route

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Http\Controllers\Api\DocumentController;
-use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CodeotpController;
+use App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\Api\DocumentController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +13,18 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('documents', DocumentController::class);
 Route::apiResource('events', EventController::class);
+
+
+// -> Reset password
+
+// -> Send email with code
+
+Route::get('/sendotp/{email}',[CodeotpController::class,'sendotp'] )->name('sendotp');
+
+// ->  Verifie email
+
+Route::get('/verifieOtp/{email}',[CodeotpController::class,'verifieotp'] )->name('verifieotp');
+
+// ->reset
+
+Route::post('resetpassword',[UserController::class,'resetpassword'] )->name('resetpassword');
